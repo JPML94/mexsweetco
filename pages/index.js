937955491c53355c9dd89a3dc109dfaa408c82db
Juan Pablo Mora Lopez <jp@tradecrafted.com>
2018-08-08 01:41:59 -0700

    added products json and Header component to Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import { getProducts } from '../lib/moltin'
+import Layout from '../components/Layout'
 
-const Home = ({ products }) => <pre>{JSON.stringify(products, '\t', 2)}</pre>
+const Home = ({ products }) => (
+  <Layout title="Home">
+    <pre>{JSON.stringify(products, '\t', 2)}</pre>
+  </Layout>
+)
 
 Home.getInitialProps = async () => {
   const products = await getProducts()
