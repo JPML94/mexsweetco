999c76c4d513e3b671a43a7c05ff89ddfeb6058b
Juan Pablo Mora Lopez <jp@tradecrafted.com>
2018-08-14 16:09:04 -0700

    added ProductSummary and ProductAttributes components to product page

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import { Container } from 'semantic-ui-react';
 import Header from './Header';
 
 export default ({ children, title = '' }) => (
-  <div>
+  <React.Fragment>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
@@ -18,5 +18,5 @@ export default ({ children, title = '' }) => (
     <Container text style={{ paddingTop: '7em' }}>
       {children}
     </Container>
-  </div>
+  </React.Fragment>
 );
--- pages/product.js
+++ pages/product.js
@@ -1,18 +1,30 @@
 import Layout from '../components/Layout';
+import ProductSummary from '../components/ProductSummary';
+import ProductAttributes from '../components/ProductAttributes';
 
 import { getProductById } from '../lib/moltin';
 
-const ProductPage = props => (
-  <Layout title="product">
-    <pre>{JSON.stringify(props, '\t', 2)}</pre>
+const ProductPage = ({ product }) => (
+  <Layout title={product.name}>
+    <ProductSummary {...product} />
+    <ProductAttributes {...product} />
   </Layout>
 );
 
 ProductPage.getInitialProps = async ({query: {id}}) => {
-  const data = await getProductById(id);
-
+  const { data, included: { main_images } } = await getProductById(id);
+  
+  const imageId = data.relationships.main_image 
+    ? data.relationships.main_image.data.id 
+    : false
+  
   return {
-    data
+    product: {
+      ...data,
+      image: imageId 
+        ? main_images.find(img => img.id === imageId).link.href
+        : '/static/moltin-light-hex.svg'
+    }
   }
 }
 
