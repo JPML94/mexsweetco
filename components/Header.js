cd3540cdd8f696ccd2b1325ea1e9ab2b24d57f0a
Juan Pablo Mora Lopez <jp@tradecrafted.com>
2018-08-14 16:15:26 -0700

    added progress bar for page loading

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,30 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import { Menu, Container, Image } from 'semantic-ui-react';
 
+Router.onRouteChangeStart = url => NProgress.start()
+Router.onRouteChangeComplete = url => NProgress.done()
+Router.onRouteChangeError = url => NProgress.done()
+
 export default () => (
-  <Menu className="orange" inverted fixed='top' size='huge'>
-    <Link href='/' prefetch passHref>
-    <Menu.Item as='a' header>
-      <Image 
-        size="mini"
-        src="/static/moltin-light-hex.svg"
-        style={{ marginRight: '1.5em'}}
-      />MexSweets
-    </Menu.Item>
-    </Link>
-  </Menu>
+  <React.Fragment>
+    <Head>
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    </Head>
+    <Menu className="orange" inverted fixed='top' size='huge'>
+      <Container text>
+        <Link href='/' prefetch passHref>
+          <Menu.Item as='a' header>
+            <Image 
+              size="mini"
+              src="/static/moltin-light-hex.svg"
+              style={{ marginRight: '1.5em'}}
+            />MexSweets
+          </Menu.Item>
+        </Link>
+      </Container>
+    </Menu>
+  </React.Fragment>
 )
